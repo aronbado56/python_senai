fcc5b029227fcc55d1069e361d9518b3c3741f0d
aronbado56 <riiick2713r@gmail.com>
2024-12-05 11:26:02 -0300

    Add files via upload
    aula dia 05/12/24 idade, altura e nome

---
+++ aula 2/auala02.py
@@ -0,0 +1,12 @@
+print('bom dia')
+nome_usuario = 'henrique' #tipo -> str = string
+idade_usuario = '19'# tipo -> int = integer idade minha 
+altura_usuario = '1,67'# tipo -> float = flutuante 
+maior_idade = True # tipo -> bool = boolean
+print('Seja bem vindo', nome_usuario)
+print ('idade_usuario', idade_usuario)
+print ('altura_usuario', altura_usuario)
+print (' maior idade', maior_idade)
+print ('sua idade é', idade_usuario, 'anos ')
+print ( 'você tem', altura_usuario, 'metros')
+print ('você é maior de idade?', maior_idade)
